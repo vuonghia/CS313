ae23d5624c7bc4d3c3ac51f9526853585561a470
Nghia Vuong <github@vuonghia.com>
2015-04-27 11:40:35 -0600

    Add more files

--- teamActivity0.js
+++ teamActivity0.js
@@ -6,16 +6,19 @@ function togVis()
    else
       results.style.visibility = "hidden";
 }
-function update()
-{
-   var results = document.getElementById("results");
 
-   var proceed;
-   proceed = confirm("Are you sure you want to proceed?");
-
-   if( proceed == true)
-   {
-   results.style.background = document.getElementById("color").value;
-       results.innerHTML = results.innerHTML +
-          document.getElementById("to_append").value;
-   }
+function handleUpdateClick(){
+	var text = document.getElementById("input").value;
+	var color = document.getElementById("color").value;
+	
+	if (confirm("Are you sure you want ot make this change?") == true){
+		appendText(text);
+		updateColor(color);		
+	}
+}
+function appendText(text){
+	document.getElementById("display").innerHTML = text;
+}
+function updateColor(color){
+	document.getElementById("display").style.backgroundColor= color;
+}
